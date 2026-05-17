c7882ce5514e7b1f11bcd3b8e8443b29aed1a68e
arpan <arpanmandal913.com>
2026-05-17 19:53:09 +0530

    space

--- library_system/ums/urls.py
+++ library_system/ums/urls.py
@@ -7,7 +7,6 @@ from .views import RegisterAPI, DeleteUserAPI, ListUsersAPI, EditUserAPI
 
 urlpatterns = [
     path("register/", RegisterAPI.as_view()),
-
     path("login/", TokenObtainPairView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
     path("delete/<uuid:user_id>/", DeleteUserAPI.as_view()),
